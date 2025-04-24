2edbc2272a3dabe573c7142e136121def39407dc
lucioacri <lucioacri@gmail.com>
2025-04-24 13:56:44 +0200

    add background img

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ axios.get("https://lanciweb.github.io/demo/api/pictures/").then((response) => {
     const date = card.date;
     const id = card.id;
     cardsRow.innerHTML += `<div class="col-sm-12 col-md-6 col-lg-4 d-flex justify-content-center">
-    <div class="card" style="width: 18rem;">
+    <div class="card" style="width: 600px;">
   <img src="${img}" class="card-img-top" alt="${id}">
   <div class="card-body">
     <p class="card-text my-card-title">${title}</p>
