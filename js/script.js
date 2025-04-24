490a9bf5c0b258d69810d3effab6039281ad44f2
lucioacri <lucioacri@gmail.com>
2025-04-24 12:51:44 +0200

    add all cards

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
 const cardsCol = document.getElementById("cards-col");
 
 axios.get("https://lanciweb.github.io/demo/api/pictures/").then((response) => {
-  const img = response.data[0].url;
-  const title = response.data[0].title;
-  const date = response.data[0].date;
-  cardsCol.innerHTML += `<div class="card" style="width: 18rem;">
-  <img src="${img}" class="card-img-top" alt="...">
+  const cards = response.data;
+  cards.forEach((card) => {
+    const img = card.url;
+    const title = card.title;
+    const date = card.date;
+    const id = card.id;
+    cardsCol.innerHTML += `<div class="card" style="width: 18rem;">
+  <img src="${img}" class="card-img-top" alt="${id}">
   <div class="card-body">
     <p class="card-text">${title}</p>
     <p class="card-text">${date}</p>
   </div>
 </div>`;
+  });
 });
