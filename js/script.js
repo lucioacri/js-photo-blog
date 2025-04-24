5db5c21399b430200d0ae06d1515bede37d55ba1
lucioacri <lucioacri@gmail.com>
2025-04-24 14:10:03 +0200

    add pin on cards

--- js/script.js
+++ js/script.js
@@ -7,14 +7,16 @@ axios.get("https://lanciweb.github.io/demo/api/pictures/").then((response) => {
     const title = card.title;
     const date = card.date;
     const id = card.id;
-    cardsRow.innerHTML += `<div class="col-sm-12 col-md-6 col-lg-4 d-flex justify-content-center">
-    <div class="card" style="width: 600px;">
-  <img src="${img}" class="card-img-top" alt="${id}">
-  <div class="card-body">
-    <p class="card-text my-card-title">${title}</p>
-    <p class="card-text my-card-date">${date}</p>
-  </div>
-</div>
-</div>`;
+    cardsRow.innerHTML += `
+    <div class="col-sm-12 col-md-6 col-lg-4 d-flex justify-content-center">
+       <div class="card my-card" style="width: 600px;">
+         <img src="./img/pin.svg" class="card-decoration" alt="pin" />
+         <img src="${img}" class="card-img-top" alt="${id}">
+        <div class="card-body">
+         <p class="card-text my-card-title">${title}</p>
+         <p class="card-text my-card-date">${date}</p>
+        </div>
+     </div>
+    </div>`;
   });
 });
