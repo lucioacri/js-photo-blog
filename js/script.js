9c0528e51d170e82d58e5f74e351871dcf690d2f
lucioacri <lucioacri@gmail.com>
2025-04-24 17:29:15 +0200

    add img selector

--- js/script.js
+++ js/script.js
@@ -8,15 +8,39 @@ axios.get("https://lanciweb.github.io/demo/api/pictures/").then((response) => {
     const date = card.date;
     const id = card.id;
     cardsRow.innerHTML += `
-    <div class="col-sm-12 col-md-6 col-lg-4 d-flex justify-content-center">
-       <div class="card my-card" style="width: 600px;">
-         <img src="./img/pin.svg" class="card-decoration" alt="pin" />
-         <img src="${img}" class="card-img-top" alt="${id}">
-        <div class="card-body">
-         <p class="card-text my-card-title">${title}</p>
-         <p class="card-text my-card-date">${date}</p>
+    <div class="col-sm-12 col-md-6 col-lg-4 my-2 d-flex justify-content-center">
+        <div class="card my-card" style="width: 18rem;">
+            <img src="./img/pin.svg" class="card-decoration" alt="pin" />
+            <img src="${img}" class="card-img-top" alt="${id}">
+            <div class="card-body">
+                <p class="card-text my-card-title">${title}</p>
+                <p class="card-text my-card-date">${date}</p>
+            </div>
         </div>
-     </div>
     </div>`;
   });
+
+  const myCards = document.querySelectorAll(".my-card");
+  const overlay = document.getElementById("overlay");
+  const closeButton = document.querySelector(".close-button");
+
+  myCards.forEach((card) => {
+    const cardImg = card.querySelector(".card-img-top").src;
+    const cardId = card.querySelector(".card-img-top").alt;
+    card.addEventListener("click", function (e) {
+      e.preventDefault();
+      overlay.classList.replace("d-none", "d-flex");
+      overlay.querySelector("img").src = cardImg;
+      overlay.querySelector("img").alt = cardId;
+    });
+  });
+
+  closeButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    overlay.classList.replace("d-flex", "d-none");
+  });
 });
+
+// cardsRow.innerHTML += `<div class="d-flex justify-content-center align-items-center fixed-div">
+// <img src="${img}" class="fixed-img" alt="${id}">
+// </div>`;
