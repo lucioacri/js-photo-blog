7d3b979616c5f3f87b8714939067856e95de5312
lucioacri <lucioacri@gmail.com>
2025-04-28 11:25:24 +0200

    add hover w transform

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ axios.get("https://lanciweb.github.io/demo/api/pictures/").then((response) => {
     const date = card.date;
     const id = card.id;
     cardsRow.innerHTML += `
-    <div class="col-sm-12 col-md-6 col-lg-4 my-2 d-flex justify-content-center">
+    <div class="col-sm-12 col-md-6 col-lg-4 my-2 d-flex justify-content-center card-col">
         <div class="card my-card" style="width: 18rem;">
             <img src="./img/pin.svg" class="card-decoration" alt="pin" />
             <img src="${img}" class="card-img-top" alt="${id}">
